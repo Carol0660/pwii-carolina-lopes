42df7bdb5f5ebd43c4d1bad43f919c14e9a27b6e
Carolina Lopes <carolcandela006@gmail.com>
2025-05-14 10:33:56 -0300

    Exercicios de php

---
+++ exercicios/ex01.php
@@ -0,0 +1,10 @@
+<html lang="pt-br">
+<body>
+        o veiculo percorreu :
+        <?php echo $_POST["km"]; ?><br>
+        utilizou de gasolina:
+        <?php echo $_POST["gasolina"]; ?>
+
+    </form>
+</body>
+</html>
