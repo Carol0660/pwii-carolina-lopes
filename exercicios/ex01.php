3482bd2b58ca3ae4aef0bbffbb5339e0fc9a7f52
Carolina Lopes <carolcandela006@gmail.com>
2025-06-04 15:01:54 -0300

    Exercicios php

--- exercicios/ex01.php
+++ exercicios/ex01.php
@@ -1,10 +1,34 @@
 <html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio01</title>
+</head>
 <body>
-        o veiculo percorreu :
-        <?php echo $_POST["km"]; ?><br>
-        utilizou de gasolina:
-        <?php echo $_POST["gasolina"]; ?>
+    <h2>Calculadora de Consumo</h2>
+    <form method="GET">
+        Informe a distancia percorrida em KM:
+        <input type="number" id="km" name="km">
+        <br>
+        Informe a quantidade de combustivel consumido em l:
+        <input type="number" id="comb" name="comb">
+        <br>
 
+        <p></p>
+
+        <input type="submit" value="Calcular">
+        <input type="reset" value="Limpar">
     </form>
 </body>
+
+    <?php
+
+        $km = 0.0;
+        $comb = 0.0;
+
+        $km = $_GET['km'];
+        $comb = $_GET['comb'];
+
+        echo ("O consumo médio de combustivel é de: " . $km / $comb . " Litros" );
+    ?>
 </html>
